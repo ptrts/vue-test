73531e701b25228f5f1639a6f5034aa7b392c0b7
Pavel Taruts <taruts@improveitgroup.com>
2019-11-21 14:34:40 +0700

    Засовываем в ветку master-copy собственно копию master

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <li class="list-group-item" v-bind:class="{completed: task.completed}">
+    <div class="row">
+      <div class="col-1">
+        <input
+          v-on:change="$emit('check-task', task.date)"
+          type="checkbox"
+          :checked="task.completed"
+        >
+      </div>
+      <div
+        v-on:click="$emit('edit-task', task.date)"
+        class="col-8 title-col"
+      >
+        {{task.title}}
+      </div>
+      <div class="col-3 delete-col">
+        <button
+          v-on:click="$emit('remove-task', task.date)"
+          type="button"
+          class="btn btn-outline-danger btn-sm"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      task: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+<style scoped>
+  .completed {
+    text-decoration: line-through;
+  }
+  .title-col {
+    max-width: 60%;
+    text-overflow: ellipsis;
+    overflow: hidden
+  }
+  .delete-col {
+    padding: 0;
+  }
+</style>
---
+++ src/components/createTask.vue
@@ -0,0 +1,36 @@
+<template>
+    <form class="form-inline" @submit.prevent="onSubmit">
+        <div class="form-group mb-2">
+            <input
+                type="text"
+                class="form-control"
+                id="taskTitle"
+                placeholder="Title"
+                v-model="title"
+            >
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Add task</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                if (this.title) {
+                    this.$emit('create-task', {
+                        title: this.title,
+                        date: Date.now(),
+                        completed: false
+                    });
+                    this.title = '';
+                }
+            }
+        }
+    }
+</script>
